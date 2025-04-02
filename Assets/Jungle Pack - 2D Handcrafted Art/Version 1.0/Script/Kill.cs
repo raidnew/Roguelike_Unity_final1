870fd397a919dacd04973f804ef8b0ce7cbae9eb
raidnew <raidnew@gmail.com>
2025-04-02 18:58:33 +0300

    *HEAD
    *HEAD

---
+++ Assets/Jungle Pack - 2D Handcrafted Art/Version 1.0/Script/Kill.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kill : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Spike")
+			Application.LoadLevel(Application.loadedLevel);
+	}
+}
---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,8 @@
+public struct Constants
+{
+    public static string horizontal = "Horizontal";
+    public static string vertical = "Vertical";
+    public static string attack1 = "Fire1";
+    public static string attack2 = "Fire2";
+    public static string jump = "Jump";
+}
