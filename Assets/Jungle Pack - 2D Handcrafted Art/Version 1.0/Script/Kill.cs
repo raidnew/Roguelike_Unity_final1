c00cc06d88476886c0f7c33029cd9a5b250a59d3
raidnew <raidnew@gmail.com>
2025-04-09 23:16:34 +0300

    *fix level
    set Z for depth

--- Assets/Jungle Pack - 2D Handcrafted Art/Version 1.0/Script/Kill.cs
+++ Assets/Jungle Pack - 2D Handcrafted Art/Version 1.0/Script/Kill.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Kill : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D coll) {
-		//if (coll.gameObject.tag == "Spike")
-			//Application.LoadLevel(Application.loadedLevel);
+		if (coll.gameObject.tag == "Spike")
+			Application.LoadLevel(Application.loadedLevel);
 	}
 }
